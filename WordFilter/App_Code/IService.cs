2a01de1dbb6d3925b88b548518da851d34427802
ovr9000storks <72412625+ovr9000storks@users.noreply.github.com>
2022-05-13 12:23:10 -0700

    Word Filter
    Upload a custom RESTful service to return the original text stream with "stop words" removed. Uses Microsoft's machine learning to more accurately tokenize words and remove stop words. Stop words are items such as articles and predicates -- words that do not give important information.

---
+++ WordFilter/App_Code/IService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
+[ServiceContract]
+public interface IService
+{
+
+	[OperationContract]
+	[WebGet(UriTemplate="removeStops?x={input}",ResponseFormat = WebMessageFormat.Json)]
+	string removeStopWords(string input);
+
+}
+
+
+[DataContract]
+public class TextData
+{
+	public string Text { get; set; }
+}
+
+[DataContract]
+public class TextTokens
+{
+	public string[] Tokens { get; set; }
+}
---
+++ WordFilter/App_Code/Service.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Microsoft.ML;
+
+
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service : IService
+    {
+        //heavily relies on the open-source Microsoft Machine Learning API
+        public string removeStopWords(string input)
+        {
+            //extend the size allowed for transfer
+            BasicHttpBinding binding = new BasicHttpBinding();
+            binding.MaxReceivedMessageSize = 2 << 20;
+
+            //create the context for the data
+            var context = new MLContext();
+
+            //create an empty data set
+            var emptyData = new List<TextData>();
+
+            //create the data
+            var data = context.Data.LoadFromEnumerable(emptyData);
+
+            //create the parameters for tokenization
+            //remove the stop words
+            //return the tokens of modified input
+            var tokenization = context.Transforms.Text.TokenizeIntoWords("Tokens", "Text", separators: new[] { ' ', '.', ',', '|' }).Append(context.Transforms.Text.RemoveDefaultStopWords("Tokens", "Tokens", Microsoft.ML.Transforms.Text.StopWordsRemovingEstimator.Language.English));
+
+            //create the data model
+            var model = tokenization.Fit(data);
+
+            //create the engine with the previously made context
+            var engine = context.Model.CreatePredictionEngine<TextData, TextTokens>(model);
+
+            //create the text with the stop words removed
+            var text = engine.Predict(new TextData { Text = input });
+
+
+            return CreateOutput(text);
+        }
+
+        public void setMaxMessageSize()
+        {
+            BasicHttpBinding binding = new BasicHttpBinding();
+
+            binding.MaxReceivedMessageSize = 2 << 20;
+        }
+
+        private string CreateOutput(TextTokens tokens)
+        {
+            var builder = new StringBuilder();
+
+            //adds the tokens and a space to the string being built 
+            foreach (var token in tokens.Tokens)
+            {
+                builder.Append(token);
+                builder.Append(" ");
+            }
+
+            return builder.ToString();
+        }
+    }
+
