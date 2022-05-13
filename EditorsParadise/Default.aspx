4d58afeb114e7560a4eb3ea63aa33d7a3af87551
ovr9000storks <72412625+ovr9000storks@users.noreply.github.com>
2022-05-13 12:13:39 -0700

    Initial Commit
    Uploaded all files regarding the .NET application and HTML. Services and packages have yet to be uploaded.

---
+++ EditorsParadise/Default.aspx
@@ -0,0 +1,92 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="EditorsParadise.Default" %>
+<%@ OutputCache Duration="180" VaryByParam="*" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server" style="border-radius:0; width:100%;">
+    <title>Editor's Paradise</title>
+    <style type="text/css">
+        #PageHeader {
+            text-align: center;
+            width: 1465px;
+        }
+        #PageTextArea {
+            text-align: center;
+            width: 1100px;
+        }
+        #PageFileUpload {
+            text-align: center;
+        }
+        #PageTextOptions {
+            text-align: center;
+            width: 1100px;
+        }
+        #Top10WordsArea {
+            text-align: center;
+            width: 1100px;
+        }
+        div{
+            background-color:#04293A;
+            border-radius:15px;
+            margin:auto;
+        }
+    </style>
+
+
+    
+    <asp:Image ID="PageLogo" runat="server" ImageUrl="~/PageContent/Images/LogoPlaceholder.png" Height="146px" Width="185px" />
+    <br /><br /><br /><br />
+</head>
+<body style="background-color:#041C32; text-align: center; color:azure">
+    <form id="form1" runat="server">
+        <div id="PageFileUpload" runat="server" style="width: 1100px"><br />
+            Select a text file, or paste your plain text in the box below<br /><br />
+            <asp:FileUpload ID="InputTextFileUpload" runat="server" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="TextUploadButton" runat="server" OnClick="TextUploadButton_Click" Text="Upload" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="ResetTextButton" runat="server" OnClick="ResetTextButton_Click" Text="Reset text" />
+            <br />
+            <br />
+            <asp:Label ID="LoadedLabel" runat="server" Text="Loaded File:" style="text-decoration: underline"></asp:Label>
+&nbsp;<asp:Label ID="LoadedFileLabel" runat="server" Text=" "></asp:Label>
+            <br /><br />
+        </div>
+        <br /><br />
+        <div id="PageTextOptions" runat="server"><br />
+            Select an operation to perform<br /><br />
+            <asp:DropDownList ID="SelectedOptionList" runat="server">
+                <asp:ListItem Selected="True" Value="top10">Top 10 Words</asp:ListItem>
+                <asp:ListItem Value="replace">Word Replacement</asp:ListItem>
+                <asp:ListItem Value="filter">Word Filter</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <br />
+            <div id="WordReplacementParameters" runat="server">
+                Replace this:&nbsp;&nbsp;&nbsp;
+                <asp:TextBox ID="TextToReplace" runat="server" Width="200px"></asp:TextBox>
+                <br />
+                With this:&nbsp;&nbsp;&nbsp;
+                <asp:TextBox ID="ReplacementText" runat="server" Width="200px"></asp:TextBox>
+                <br /><br />
+            </div>
+            <asp:Button ID="ActionButton" runat="server" Text="Ship it!" OnClick="ActionButton_Click" />
+            <br /><br />
+        </div>
+        <br /><br />
+        <div id="PageTextArea" runat="server">
+            <div id="Top10WordsArea" runat="server" visible="false"><br />
+                Top 10 words<br />
+                <br />
+                <textarea id="Top10WordsOut" runat="server" cols="60" name="Top10WordsOut" rows="10"></textarea><br />
+            </div>
+            <br />Displayed text:<br /><br />
+            <textarea id="MainTextArea" runat="server" cols="120" name="MainTextArea" rows="25"></textarea><br /><br />
+        </div>
+        <textarea id="SavedText" runat="server" hidden="hidden" ></textarea>
+        <textbox id="SavedFileName" runat="server" hidden="hidden"></textbox>
+        <br /><br />
+    </form>
+</body>
+</html>
---
+++ EditorsParadise/Default.aspx.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+
+namespace EditorsParadise
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        bool validFileType = true;
+        bool fileLoaded = false;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+
+            HttpCookie cookie = Request.Cookies["myCookie"];
+            if(cookie != null)
+            {
+                MainTextArea.InnerText = cookie["textArea"];
+                LoadedFileLabel.Text = cookie["fileName"];
+            }
+        }
+
+        protected void ResetTextButton_Click(object sender, EventArgs e)
+        {
+            if(SavedText.InnerText.Length > 0)
+            {
+                MainTextArea.InnerText = SavedText.InnerText;
+                LoadedFileLabel.Text = SavedFileName.InnerText;
+                Top10WordsArea.Visible = false;
+            }
+        }
+
+        protected void ActionButton_Click(object sender, EventArgs e)
+        {
+            if(MainTextArea.InnerText != "")
+            {
+                if (SelectedOptionList.SelectedValue == "top10")
+                {
+                    Top10WordsArea.Visible = true;
+                    outputTop10Strings(MainTextArea.InnerText);
+                }
+                else
+                {
+                    //hide the top 10 words div
+                    Top10WordsArea.Visible = false;
+
+
+                    if (SelectedOptionList.SelectedValue == "replace")
+                    {
+                        outputReplacedString(MainTextArea.InnerText, TextToReplace.Text, ReplacementText.Text);
+                    }
+                    else if (SelectedOptionList.SelectedValue == "filter")
+                    {
+                        outputFilteredString(MainTextArea.InnerText);
+                    }
+                    else
+                    {
+                        MainTextArea.InnerText = "Selection Error!\nReload the page or contact the service provider";
+                    }
+                }
+
+                HttpCookie cookie = new HttpCookie("myCookie");
+                cookie["textArea"] = MainTextArea.InnerText;
+                cookie["fileName"] = LoadedFileLabel.Text;
+                cookie.Expires = DateTime.Now.AddMinutes(2);
+                Response.Cookies.Add(cookie);
+
+            }
+
+
+
+
+
+        }
+
+        protected void TextUploadButton_Click(object sender, EventArgs e)
+        {
+            if (InputTextFileUpload.HasFile)
+            {
+                string filename = Server.HtmlEncode(InputTextFileUpload.FileName);
+                string extension = System.IO.Path.GetExtension(filename);
+
+                LoadedFileLabel.Text = filename;
+
+                if(extension == ".txt")
+                {
+                    validFileType = true;
+                    SavedText.InnerText = System.Text.Encoding.Default.GetString(InputTextFileUpload.FileBytes);
+                    MainTextArea.InnerText = SavedText.InnerText;
+                    fileLoaded = true;
+                    SavedFileName.InnerText = filename;
+                }
+                else
+                {
+                    validFileType = false;
+                    MainTextArea.InnerText = "File is not a plain text (.txt) file\nPlease upload a plain text file";
+                }
+
+
+            }
+            else
+            {
+                MainTextArea.InnerText = "No file selected\nPlease select a file before performing an action";
+                LoadedFileLabel.Text = "";
+            }
+
+
+            //Add the content of the main text area to the cookie list
+            //Expires in 2 minutes
+            HttpCookie cookie = new HttpCookie("myCookie");
+            cookie["textArea"] = MainTextArea.InnerText;
+            cookie["fileName"] = LoadedFileLabel.Text;
+            cookie.Expires = DateTime.Now.AddMinutes(2);
+            Response.Cookies.Add(cookie);
+
+        }
+
+        protected string webFriendlySpace(string input)
+        {
+            return input.Replace(" ", "|");
+        }
+
+        protected void outputTop10Strings(string input)
+        {
+            string x = "x=" + webFriendlySpace(input);
+            string apiCall = @"http://localhost:62514/Service.svc/top10Strings?" + x;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiCall);
+            WebResponse response = request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            String rawJSON = reader.ReadToEnd();
+
+            List<KeyValuePair<string, int>> top10List = JsonConvert.DeserializeObject<List<KeyValuePair<string, int>>>(rawJSON);
+
+            foreach(var pair in top10List)
+            {
+                Top10WordsOut.InnerText = Top10WordsOut.InnerText + pair.Key + " (" + pair.Value + ")\n";
+            }
+        }
+
+        protected void outputReplacedString(string input, string changeThis, string toThis)
+        {
+            string x = "x=" + webFriendlySpace(input);
+            string y = "y=" + webFriendlySpace(changeThis);
+            string z = "z=" + webFriendlySpace(toThis);
+            string apiCall = @"http://localhost:62516/Service.svc/replaceString?" + x + "&" + y + "&" + z;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiCall);
+            WebResponse response = request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            String rawJSON = reader.ReadToEnd();
+
+            MainTextArea.InnerText = JsonConvert.DeserializeObject<string>(rawJSON);
+        }
+
+        protected void outputFilteredString(string input)
+        {
+            string x = "x=" + webFriendlySpace(input);
+            string apiCall = @"http://localhost:62517/Service.svc/removeStops?" + x;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiCall);
+            WebResponse response = request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            String rawJSON = reader.ReadToEnd();
+
+            MainTextArea.InnerText = JsonConvert.DeserializeObject<string>(rawJSON);
+        }
+    }
+}
